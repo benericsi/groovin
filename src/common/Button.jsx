8a7c2279a94d8ceeb0748fda664992e426d5b01f
benericsi <bene.richard24@gmail.com>
2023-09-24 14:23:23 +0200

    own button component wuth style

---
+++ src/common/Button.jsx
@@ -0,0 +1,14 @@
+import '../assets/css/button.css';
+
+import React from 'react';
+
+const Button = (props) => {
+  const className = props.className ? `btn-main ${props.className}` : 'btn-main';
+  return (
+    <button className={className} onClick={props.onClick} type={props.type}>
+      {props.text}
+    </button>
+  );
+};
+
+export default Button;
