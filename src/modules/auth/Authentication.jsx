e3be3760a54b52d4ac3f912a3395132d60197e85
benericsi <bene.richard24@gmail.com>
2023-09-25 18:50:46 +0200

    minor changes to toast and dashboard

--- src/modules/auth/Authentication.jsx
+++ src/modules/auth/Authentication.jsx
@@ -1,8 +1,13 @@
+import {useEffect} from 'react';
 import '../../assets/css/authentication.css';
 
 import AuthTabs from './AuthTabs';
 
 const Authentication = () => {
+  useEffect(() => {
+    document.title = 'Authentication | Groovin.';
+  }, []);
+
   return (
     <>
       <div className="auth-page">
