4e1110db1a57229e39b2d45e526c294ad12e65af
benericsi <bene.richard24@gmail.com>
2023-10-26 12:52:09 +0200

    login logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './assets/css/globals.css';
+
+import {Routes, Route} from 'react-router-dom';
 import Authentication from './modules/authentication/Authentication';
 
 function App() {
-  return <Authentication />;
+  return (
+    <Routes>
+      <Route path="/auth" element={<Authentication />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/modules/authentication/Login.jsx
+++ src/modules/authentication/Login.jsx
@@ -10,6 +10,8 @@ import {useDebounce} from '../../hooks/useDebounce';
 import {useNavigate} from 'react-router-dom';
 import {useLoader} from '../../hooks/useLoader';
 import {useToast} from '../../hooks/useToast';
+import {useAuth} from '../../hooks/useAuth';
+import {db} from '../../setup/Firebase';
 
 import {FaGoogle} from 'react-icons/fa';
 
@@ -17,6 +19,7 @@ const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 const Login = () => {
   const navigate = useNavigate();
+  const {login, loginWithGoogle} = useAuth();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -43,8 +46,9 @@ const Login = () => {
     [email]
   );
 
-  const handleFormSubmit = async (e) => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
+    showLoader();
 
     if (!email || !password) {
       addToast('error', 'Please fill out all the fields!');
@@ -55,6 +59,57 @@ const Login = () => {
       addToast('error', 'There is an error with at least one field!');
       return;
     }
+
+    login(email, password)
+      .then(async (cred) => {
+        setEmail('');
+        setPassword('');
+
+        hideLoader();
+        addToast('success', 'You have successfully logged in!');
+        navigate('/');
+      })
+      .catch((error) => {
+        hideLoader();
+        if (error.code === 'auth/user-not-found') {
+          addToast('error', 'There is no user with this e-mail.');
+        } else if (error.code === 'auth/wrong-password') {
+          addToast('error', 'Wrong password.');
+        } else if (error.code === 'auth/invalid-login-credentials') {
+          addToast('error', 'Invalid login credentials.');
+        } else {
+          addToast('error', error.message);
+        }
+      });
+  };
+
+  const handleLoginWithGoogle = (e) => {
+    e.preventDefault();
+    showLoader();
+
+    loginWithGoogle()
+      .then(async (cred) => {
+        if (cred.additionalUserInfo.isNewUser) {
+          await db.collection('users').doc(cred.user.uid).set({
+            firstName: cred.additionalUserInfo.profile.given_name,
+            lastName: cred.additionalUserInfo.profile.family_name,
+            displayName: cred.user.displayName,
+            email: cred.user.email,
+            photoURL: cred.user.photoURL,
+            friends: [],
+          });
+        }
+
+        // Signup was successful
+        addToast('success', 'You have successfully signed in!');
+        navigate('/');
+      })
+      .catch((error) => {
+        addToast('error', error.message);
+      })
+      .finally(() => {
+        hideLoader();
+      });
   };
 
   return (
@@ -62,7 +117,7 @@ const Login = () => {
       <h1>Welcome Back!</h1>
       <div className="social-signup">
         <h4>Connect with</h4>
-        <Button text="Google" className="dark">
+        <Button text="Google" className="dark" onClick={handleLoginWithGoogle}>
           <FaGoogle />
         </Button>
       </div>
