4e63c91754d8a9fa6407e2f22dbfbe5d738f2e55
benericsi <bene.richard24@gmail.com>
2023-11-23 17:21:16 +0100

    init messages page

--- src/modules/messages/Messages.jsx
+++ src/modules/messages/Messages.jsx
@@ -1,11 +1,20 @@
+import '../../assets/css/messages.css';
+
 import {useAccessControl} from '../../hooks/useAccessControl';
 import {useTitle} from '../../hooks/useTitle';
+import UserList from './UserList';
+import ChatRoom from './ChatRoom';
 
 const Messages = () => {
   useAccessControl();
   useTitle('Messages');
 
-  return <h1>Messages</h1>;
+  return (
+    <section className="messages-section">
+      <UserList />
+      <ChatRoom />
+    </section>
+  );
 };
 
 export default Messages;
