d126814c6c192a1df9db131fd9943cc9207b4759
Ben Richárd <bene.richard24@gmail.com>
2024-10-28 20:57:37 +0100

    dashboard

--- src/modules/categories/Category.jsx
+++ src/modules/categories/Category.jsx
@@ -1,28 +1,31 @@
-import '../../assets/css/categories.css';
-import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
-import {useSpotifyAuth} from '../../hooks/useSpotifyAuth';
-import {useToast} from '../../hooks/useToast';
-import {useLoader} from '../../hooks/useLoader';
-import {Link} from 'react-router-dom';
+import "../../assets/css/categories.css";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSpotifyAuth } from "../../hooks/useSpotifyAuth";
+import { useToast } from "../../hooks/useToast";
+import { useLoader } from "../../hooks/useLoader";
+import { Link } from "react-router-dom";
 
 const Category = () => {
   const [category, setCategory] = useState({});
 
-  const {categoryId} = useParams();
+  const { categoryId } = useParams();
   const token = useSpotifyAuth();
-  const {addToast} = useToast();
-  const {showLoader, hideLoader} = useLoader();
+  const { addToast } = useToast();
+  const { showLoader, hideLoader } = useLoader();
 
   useEffect(() => {
     if (!token) return;
     showLoader();
-    fetch(`https://api.spotify.com/v1/browse/categories/${categoryId}/playlists?offset=0&limit=20&locale=en-EN`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    fetch(
+      `https://api.spotify.com/v1/browse/categories/${categoryId}/playlists?offset=0&limit=20&locale=en-EN`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
       .then((response) => response.json())
       .then((data) => {
         // console.log(data);
@@ -30,7 +33,7 @@ const Category = () => {
         hideLoader();
       })
       .catch((error) => {
-        addToast('error', error.message);
+        addToast("error", error.message);
         hideLoader();
       })
       .finally(() => {
@@ -41,22 +44,38 @@ const Category = () => {
   }, [token, categoryId]);
 
   return (
-    <div className="categories-body">
-      <section className="categories-section">
+    <div className='categories-body'>
+      <section className='categories-section'>
         {category && (
           <>
-            <section className="body-section">
-              <div className="list-header">
+            <section className='body-section'>
+              <div className='list-header'>
                 <h2>{category.message}</h2>
               </div>
-              {category.playlists && category.playlists.items && category.playlists.items.length === 0 && <h1 className="no-data"> This category does not have any playlists yet. </h1>}
-              <div className="search-list">
+              {category.playlists &&
+                category.playlists.items &&
+                category.playlists.items.length === 0 && (
+                  <h1 className='no-data'>
+                    {" "}
+                    This category does not have any playlists yet.{" "}
+                  </h1>
+                )}
+              <div className='search-list'>
                 {category.playlists &&
                   category.playlists.items.map((playlist, index) => (
-                    <Link to={`/category/playlist/${playlist.id}`} className="search-card" key={index}>
-                      <img className="search-card-photo playlist" src={playlist.images[0].url} alt={playlist.name} />
-                      <div className="search-card-name">{playlist.name}</div>
-                      <div className="search-card-info capitalize">{playlist.type}</div>
+                    <Link
+                      to={`/category/playlist/${playlist.id}`}
+                      className='search-card'
+                      key={index}>
+                      <img
+                        className='search-card-photo playlist'
+                        src={playlist.images[0].url}
+                        alt={playlist.name}
+                      />
+                      <div className='search-card-name'>{playlist.name}</div>
+                      <div className='search-card-info capitalize'>
+                        {playlist.type}
+                      </div>
                     </Link>
                   ))}
               </div>
--- src/modules/main/Dashboard.jsx
+++ src/modules/main/Dashboard.jsx
@@ -1,15 +1,91 @@
-//import '../../assets/css/dashboard.css';
+import "../../assets/css/dashboard.css";
 
-import React from 'react';
-import {useTitle} from '../../hooks/useTitle';
+import React, { useEffect, useState } from "react";
+import { useTitle } from "../../hooks/useTitle";
+import { useToast } from "../../hooks/useToast";
+import { useLoader } from "../../hooks/useLoader";
+import { useSpotifyAuth } from "../../hooks/useSpotifyAuth";
+import { useAuth } from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  useTitle('Home');
+  useTitle("Home");
+
+  const [releases, setReleases] = useState([]);
+
+  const { currentUser } = useAuth();
+
+  console.log(currentUser);
+
+  const token = useSpotifyAuth();
+
+  const { addToast } = useToast();
+  const { showLoader, hideLoader } = useLoader();
+
+  useEffect(() => {
+    const fetchNewReleases = async () => {
+      showLoader();
+
+      try {
+        const response = await fetch(
+          "https://api.spotify.com/v1/browse/new-releases?limit=50",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const data = await response.json();
+
+        setReleases(data.albums.items);
+      } catch (error) {
+        addToast("error", error.message);
+      } finally {
+        hideLoader();
+      }
+    };
+
+    if (token) {
+      fetchNewReleases();
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token]);
 
   return (
-    <>
-      <h1>Welcome To The Dashboard!</h1>
-    </>
+    <div className='dashboard-body'>
+      <section className='dashboard-section'>
+        {releases.length > 0 && (
+          <section className='body-section'>
+            <div className='list-header'>
+              <h2>
+                Hi, {currentUser.displayName.split(" ")[0]}! Listen to the top
+                releases now
+              </h2>
+            </div>
+            <div className='search-list'>
+              {releases.map((release, index) => (
+                <Link
+                  to={`/album/${release.id}`}
+                  className='search-card'
+                  key={index}>
+                  <img
+                    className='search-card-photo album'
+                    src={release.images[0].url}
+                    alt={release.name}
+                  />
+                  <div className='search-card-name'>{release.name}</div>
+                  <div className='search-card-info capitalize'>
+                    {release.artists.map((artist) => artist.name).join(", ")}
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </section>
+        )}
+      </section>
+    </div>
   );
 };
 
